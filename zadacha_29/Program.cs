86a7624797c1fd65fae1f83c8bcc4bf078c18f42
sergey.k <k.sergey2010@yandex.ru>
2022-04-25 19:14:48 +0700

    zadacha_29 done

---
+++ zadacha_29/Program.cs
@@ -0,0 +1,23 @@
+﻿void Vvod (int[] array)
+{
+    Console.WriteLine("Введите элементы массива через Enter");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+}
+
+void Print (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.WriteLine("Введите количество элементов в массиве");
+int coll = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[coll];
+Vvod(arr);
+Print(arr);
