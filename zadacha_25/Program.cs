531ee0602b1743dbe71df8d29e5179df8941aaa5
sergey.k <k.sergey2010@yandex.ru>
2022-04-25 18:28:06 +0700

    zadacha_25 done

---
+++ zadacha_25/Program.cs
@@ -0,0 +1,11 @@
+﻿int Stepen (int osnova, int stepen)
+{
+    int result = 1;
+    for (int i = 0; i < stepen; i++)
+    {
+        result = result * osnova;
+    }
+    return result;
+}
+
+System.Console.WriteLine(Stepen(3, 5));
