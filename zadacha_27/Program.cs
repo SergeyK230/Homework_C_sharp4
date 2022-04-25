d027224d3794e3084b1b720e1d41ca4df902bc36
sergey.k <k.sergey2010@yandex.ru>
2022-04-25 18:49:00 +0700

    zadacha_27 done

---
+++ zadacha_27/Program.cs
@@ -0,0 +1,12 @@
+﻿int Summa(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return Math.Abs(result);
+}
+
+System.Console.WriteLine(Summa(-65));
